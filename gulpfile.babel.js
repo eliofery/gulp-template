5d62b58d00c0089ea3c5ebca494478d4a80e3361
Sergey Erofeev <erofsey@yandex.kz>
2023-07-26 21:52:01 +0600

    add server task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,7 @@ import { series } from 'gulp'
 
 // Таски
 import clear from './gulp/tasks/clear'
+import server from './gulp/tasks/server'
 
 // Конфиги
 import config from './gulp/config'
@@ -10,10 +11,13 @@ import config from './gulp/config'
 // Определяем окружения сборки dev или prod
 config.setEnv()
 
+// Запуск виртуального сервера
+export const proxy = server
+
 // Сборка проекта
 export const build = series(clear)
 
 // Слежение за изменением файлов
-export const watch = series(build)
+export const watch = series(build, server)
 
 export default watch
